78550301d31d2f5bacc9fa25653a6f733ba3b0fd
Christopher Marzulli <chriszulli15@gmail.com>
2022-11-09 14:27:57 -0500

    commit tree d82a2ac571b117f5886b4095b76d6dfbf1a20374

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # resources :users, only:[:show, :create]
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
